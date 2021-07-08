1dc32bfa7ed8c8828fa0d6f6c7af8b0fbcffb271
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-08 14:25:05 -0400

    commit tree 3fdd526fe73f4464678eadebae7690cb56bbab12

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ActiveRecord::Base
-  
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+
+  validates :name, :content, presence: true
+#   accepts_nested_attributes_for :tags
+
+  def tags_attributes=(tag_attributes)
+    tag_attributes.values.each do |tag_attribute|
+        if tag_attribute[:name].present?
+            tag = Tag.find_or_create_by(tag_attribute)
+        self.tags << tag
+        end
+    end
+    end
+
 end
